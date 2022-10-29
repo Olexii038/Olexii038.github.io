73eead6d3f787db0e203960dad4a13c20094e730
Olexii038 <116962735+Olexii038@users.noreply.github.com>
2022-10-29 18:04:46 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 
 
 
+var lowernumber = -10;
+var upperNumber = 10;
+
 function start() {
 	generateNewPoint()
+}
+
+function setBorders() {
+	var input1 = document.getElementById("input1")
+	var input2 = document.getElementById("input2")
+	changeBorders(input1.value, input2.value)
+	input1.value = ""
+	input2.value = ""
+}
+
+function changeBorders(value1, value2) {
+	if (value1 == "" || value2 == "") {
+		return
+	}
+	num1 = Number(value1)
+	num2 = Number(value2)
+	if (isNaN(num1) || isNaN(num2)) {
+		return
+	}
+	if (num1 >= num2) {
+		return
+	}
+	lowernumber = num1
+	upperNumber = num2
 }
 
 function generateNewPoint() {
@@ -12,8 +39,8 @@ function generateNewPoint() {
 
 function getRandomNumbers() {
 	var nums = []
-	nums[0] = Math.floor((20 * Math.random() - 10))
-	nums[1] = Math.floor((20 * Math.random() - 10))
+	nums[0] = Math.floor(((upperNumber - lowernumber) * Math.random() + lowernumber))
+	nums[1] = Math.floor(((upperNumber - lowernumber) * Math.random() + lowernumber))
 	var result = "(" + nums[0] + ", " + nums[1] + ")"
 	return result 
 }
